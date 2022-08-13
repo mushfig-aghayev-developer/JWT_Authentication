9e6e27a874e635b183fb552bc1e726a279eda85b
mushfig-agahyev-developer <webprogrammer.readonly@gmail.com>
2022-08-13 19:55:26 +0400

    ok

--- Authorize/Startup.cs
+++ Authorize/Startup.cs
@@ -50,8 +50,6 @@ namespace Authorize
             });
         }
 
-
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
